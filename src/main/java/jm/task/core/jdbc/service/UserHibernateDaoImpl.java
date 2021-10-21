66f42593d22fa1e1ec4f99d03929e952d9aca039
Evgeniy Kazban <evgeny.kazban3.0@yandex.ru>
2021-10-21 17:16:51 +0300

    UserHibernateDaoImpl methods

--- src/main/java/jm/task/core/jdbc/service/UserHibernateDaoImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserHibernateDaoImpl.java
@@ -1,6 +1,45 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
 
-public class UserHibernateDaoImpl extends UserDaoHibernateImpl implements UserService {
+import java.util.List;
+
+public class UserHibernateDaoImpl implements UserService {
+    private UserDao userDao;
+
+    public UserHibernateDaoImpl() {
+        userDao = new UserDaoHibernateImpl();
+    }
+
+    @Override
+    public void createUsersTable() {
+        userDao.createUsersTable();
+    }
+
+    @Override
+    public void dropUsersTable() {
+        userDao.dropUsersTable();
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        userDao.saveUser(name, lastName, age);
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        userDao.cleanUsersTable();
+    }
 }
